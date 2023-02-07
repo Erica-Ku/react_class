ba307c0a91e0adf5e4bd6a8125af0b48aa5d149f
Erica-Ku <yeskuminji@naver.com>
2023-02-07 13:14:51 +0900

    07

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import Gal from './06/Gal';
+//import Gal from './06/Gal';
+import Mycom from './07/Mycom';
 import './App.css';
 //import Taccident from './05/Taccident';
 
-const App = () => {
 
+const App = () => {
   return (   
-      <Gal />
+      <Mycom />
   );
 }
 
